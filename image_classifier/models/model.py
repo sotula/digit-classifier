055d799836e2ce3edd7e42f282695bb306d289b2
sotula <sotulams@gmail.com>
2023-03-09 19:50:56 +0200

    add image_classifier template

---
+++ image_classifier/models/model.py
@@ -0,0 +1,17 @@
+"""
+Module with base class for digit classifier models
+"""
+# from abc import ABC, abstractmethod
+
+class DigitClassificationInterface():
+    """
+    Base class for digit classifier models
+    """
+    __model_details: dict
+    __fit_params: dict
+
+    def __init__(self):
+    	print('hello')
+
+
+
---
+++ main.py
@@ -0,0 +1,7 @@
+from image_classifier import DigitClassificationInterface
+
+def main():
+    DigitClassificationInterface()
+
+if __name__ == "__main__":
+    main()
